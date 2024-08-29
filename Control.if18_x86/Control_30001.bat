cc2e36d679968d062cd54178e5ca179b3ff24723
Mahmood Kiasari <mtk1793@gmail.com>
2024-08-29 08:43:14 -0300

    Initilizae 2

---
+++ Control.if18_x86/Control_30001.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\Intel\oneAPI\compiler\2024.2\..\..\setvars.bat"  ia32
+pushd "G:\My Drive\PhD\PSCAD\Learning\Git Learning Examples\PSCAD--Learning\Control.if18_x86\"
+Control.exe -v5 -config Control_1.config
+popd
---
+++ Control.if18_x86/Control_mak.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\Intel\oneAPI\compiler\2024.2\..\..\setvars.bat"  ia32
+pushd "G:\My Drive\PhD\PSCAD\Learning\Git Learning Examples\PSCAD--Learning\Control.if18_x86\"
+nmake -f Control.mak
+popd
